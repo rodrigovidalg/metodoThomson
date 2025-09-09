4d61e6c323c3059138d8782aa6790bbf9910daee
RodrigoVidalGuzmanDiaz <rguzmand2@miumg.edu.gt>
2025-09-08 18:39:10 -0600

    se agrega el codigo para la tarea 5 de lenguajes formales y automatas

---
+++ programa.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar metodoThomson-1.0-SNAPSHOT.jar
+pause
---
+++ src/main/java/com/mycompany/metodothomson/MetodoThomson.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.metodothomson;
+
+/**
+ *
+ * @author user
+ */
+public class MetodoThomson {
+
+    public static void main(String[] args) {
+        frm_metodoThomson frm = new frm_metodoThomson();
+        frm.show();
+    }
+}
